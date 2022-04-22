4e429748d51311e6340b7fb0ff16989cd0978930
Chérie Oduwole <bellapette0@gmail.com>
2022-04-22 06:11:30 +0000

    Implement admin dashboard count and hard-code admin username and password instead of pulling from env file due to error

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+  http_basic_authenticate_with name: 'Jungle', password: 'book'
 
   def show
+    @count_product = Product.all 
+    @count_category = Category.all
   end
 end
