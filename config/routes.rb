db2f67310968d2e6aabb6a5f014d41d1fe6bb7aa
Chérie Oduwole <bellapette0@gmail.com>
2022-04-26 01:15:07 +0000

    Implement user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,11 @@ Rails.application.routes.draw do
 
 
   get  '/about', to: 'about#show'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
